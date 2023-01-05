5369906adb328797c94715764ad9819ab5ec5fe7
kasun beddewela <kbeddewela@aampglobal.com>
2023-01-05 05:52:35 -0500

    added more example/test projects to the soulutio

---
+++ AsyncAwaitPitfalls/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace AsyncAwaitPitfalls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        /* we can of course await on a function that returns task*/
+        Task SleepTaskAsyn(int count)
+        {
+            return Task.Run(() => { Thread.Sleep(count); });
+        }
+        private int flag = 1;
+        async void startSleepAndReturn(int count)
+        {
+            if (flag == 1)
+            {
+                await SleepTaskAsyn(count);
+                Console.WriteLine("sleep Completed:");
+            }
+            else
+            {
+                Console.WriteLine("Flag is not 1");
+            }
+            Console.WriteLine("Flag check completed");
+        }
+        /*
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Button Click start");
+            startSleepAndReturn(5000);
+
+            Console.WriteLine("Button Click stop");
+        }*/
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.flag = 1;
+            Console.WriteLine("Button 1 Click start");
+            startSleepAndReturn(5000);
+
+            Console.WriteLine("Button 1 Click stop");
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            this.flag = 0;
+            Console.WriteLine("Button 2 Click start");
+            startSleepAndReturn(5000);
+
+            Console.WriteLine("Button 2 Click stop");
+        }
+    }
+}
---
+++ FileCRCCalculation/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace FileCRCCalculation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string FileLocation = "c:\\Projects\\PC_Software\\UWFU_GitLab\\ufwu_application\\UniversalFreescaleUpdater\\bin\\" +
+                                    "Debug_Dev\\UpdateFiles\\FCIJL18\\1.1.2.1\\FCIJL18_V1.pufx";
+        string FileLocation2 = "c:\\Projects\\PC_Software\\UWFU_GitLab\\ufwu_application\\UniversalFreescaleUpdater\\bin\\" +
+                                "Debug_Dev\\UpdateFiles\\BBY-RP5-GM31-Firmwar\\5.7.4.1\\fw_A-109_Bootloader_Updater_V5.0.pufx";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            uint crc_calc = calculateCRC(FileLocation2);
+            Console.WriteLine("CRC value:" + crc_calc.ToString("X"));
+        }
+
+        uint calculateCRC(string fileName)
+        {
+            CRC32_FileCheck crc32 = new CRC32_FileCheck();
+            String crcValue = String.Empty;
+            uint localDirSize = 0;
+
+            using (FileStream fs = File.Open(fileName, FileMode.Open))
+            foreach (byte b in crc32.ComputeHash(fs))
+            {
+                crcValue += b.ToString("x2").ToLower();
+            }
+            localDirSize = Convert.ToUInt32(crcValue, 16);
+            return localDirSize;
+        }
+
+    }
+}
