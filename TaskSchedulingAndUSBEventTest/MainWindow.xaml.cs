55d2988ca96a966c2481ed0871d7f9f4bed61ec2
kasun beddewela <kbeddewela@aampglobal.com>
2023-09-10 20:41:03 -0400

    1)Finding context of WndProc() -> UsbDeviceEvent()
    2) made teh changes in the MainWindow.xaml.cs to test the effects of UI thread busyness to the USB event WndProc() generation
    
    3)reduced the sleep delay in the UpdateTaskUsingDispatch() to make the UI thread utilization more

--- TaskSchedulingAndUSBEventTest/MainWindow.xaml.cs
+++ TaskSchedulingAndUSBEventTest/MainWindow.xaml.cs
@@ -13,13 +13,34 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Interop;
+using System.Threading;
 
 namespace TaskSchedulingTest
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-
+    /*
+     * The test:
+     * 
+     * I want to demonstrate how the WndProc() gets blocked if there is heavy usage of the UI thread.
+     * To keep the UI thread bussy I will inititate a background task UpdateTaskUsingDispatch. Which will run the
+     * printOnUIThread() function on the UI thread by using theDispatcher.BeginInvoke().
+     * The printOnUIThread() has a sleep function delay of which will block the UI thread for longer, So making the
+     * sleep delay longer will increase the usage of the UI thread. 
+     * The delay in sleep function call inside the UpdateTaskUsingDispatch() will decrease the number of beginInvokes to the UI
+     * thread therefore decrase the usage of the UI thread to certain extent.
+     * 
+     * 
+     * Observation:
+     * We clearly see that the when the UI thread is uitlized heavyly the number of times WndPRoc() gets called when connecting or disconnecting USb device gets
+     * reduced to 1 instead of 2. The observable delay between when we connect the USB device and the actual WndProc()  call also get increased as the UI thread becomes more utilized/bussy.
+     * 
+     * 
+     * 
+     * 
+     */
     public delegate void UsbDeviceChange(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled);
     public partial class MainWindow : Window
     {
@@ -30,6 +51,17 @@ namespace TaskSchedulingTest
 
         public MainWindow()
         {
+
+            Thread thread = Thread.CurrentThread;
+            if (thread.Name == null)
+            {
+                Console.WriteLine("In MainWindow Thread Name is null");
+                thread.Name = "MainWindowTask";
+            }
+            else
+            {
+                Console.WriteLine("In MainWindow Thread Name is: {0}", thread.Name);
+            }
             InitializeComponent();
             UsbDeviceEvent += UsbDeviceChangeStartTask;
         }
@@ -49,11 +81,25 @@ namespace TaskSchedulingTest
         {
             if (msg == WM_DEVICECHANGE)
             {
+                Thread thread = Thread.CurrentThread;
+                if (thread.Name == null)
+                {
+                    Console.WriteLine("In WndProc Thread Name is null");
+                    thread.Name = "WndProcThread";
+                }
+                else
+                {
+                    Console.WriteLine("In WndProc Thread Name is: {0}", thread.Name);
+                }
+
                 // Check if a device has been changed
                 if ((int)wParam == DBT_DEVNODES_CHANGED)
                 {
                     Console.WriteLine("Main - Device Changed | " + hwnd + " | " + msg + " | " + wParam + " | " + lParam);
                     UsbDeviceEvent?.Invoke(hwnd, msg, wParam, lParam, ref handled);
+                    /*UsbDeviceChangeStartTask(hwnd, msg, wParam, lParam, ref handled);*/
+
+
                 }
             }
             return IntPtr.Zero;
@@ -61,8 +107,21 @@ namespace TaskSchedulingTest
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("button click");
-            taskCallBack += print_message;
+            Thread thread = Thread.CurrentThread;
+            if (thread.Name == null)
+            {
+                Console.WriteLine("In Button click Thread Name is null");
+                thread.Name = "Button_Click_Thread";
+            }
+            else
+            {
+                Console.WriteLine("In Button click Thread Name is: {0}", thread.Name);
+            }
+
+
+
+           // Console.WriteLine("button click");
+            //taskCallBack += print_message;
             Task.Factory.StartNew(() => UpdateTaskUsingDispatch());
             //Task.Factory.StartNew(() => UpdateTaskA());
             //Task.Factory.StartNew(() => UpdateTaskB());
@@ -84,19 +143,22 @@ namespace TaskSchedulingTest
             for (int i = 0; i < 1000; i++)
             {
                 //Console.WriteLine("count: " + i);
-                Dispatcher.BeginInvoke(new Action(() =>
+               Dispatcher.BeginInvoke(new Action(() =>
                {
                    printOnUIThread("usingDispatch:" + i);
 
                }));
-                System.Threading.Thread.Sleep(100);
+               System.Threading.Thread.Sleep(100);
             }
         }
 
         void printOnUIThread(string s)
         {
-            Console.WriteLine(s);
-            System.Threading.Thread.Sleep(2000);
+            Thread thread = Thread.CurrentThread;
+
+            Console.WriteLine("Print on UI Thread Name is: {0} "+s, thread.Name);
+            //Console.WriteLine(s);
+            System.Threading.Thread.Sleep(5000);
         }
 
         private void UpdateTaskA()
