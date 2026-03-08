3b557ffceb619051bb7b828a99ad50e637d7d9fa
kasun beddewela <kbeddewela@aampglobal.com>
2026-03-08 18:31:08 -0400

    1) Created and added AwiatOnPeriodicTimerAndEventWaitHandle to the solution. 2) Implemented Task loop with Task delay to demonstrate periodic work exection. 3) Update .gitignore to  ignore objets 3) TODO:  Implement task execution either on period time or EventWaitHandle signal.

---
+++ AwiatOnPeriodicTimerAndEventWaitHandle/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AwiatOnPeriodicTimerAndEventWaitHandle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private CancellationTokenSource _cts;
+
+        void DoWork()
+        {
+            Console.WriteLine($"DoWork @ {DateTime.Now}");
+        }
+        /* What chatGPT suggested was PeriodicTimer.
+         * Ex: var timer = new PeriodicTimer(TimeSpan.FromSeconds(5));
+         * But that is not avaialble in .NET framwork 4.8. So
+         we are going to use Task Loop with Task.Delay*/
+        public async Task RunAsync(CancellationToken token)
+        {
+
+            while (token.IsCancellationRequested == false)
+            {
+                DoWork();
+                await Task.Delay(1000);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this._cts = new CancellationTokenSource();
+            
+            Task.Run(() => RunAsync(_cts.Token));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _cts.Cancel();
+        }
+    }
+}
